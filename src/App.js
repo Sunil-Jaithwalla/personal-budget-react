8a7795417f13252baa0bda78caf3c451f2072410
Sunil Jaithwalla <sjaithwa@uncc.edu>
2023-09-28 02:10:47 -0400

    Made Code Changes as per the Video of Exercise 03

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Menu from './Menu/menu';
+import Hero from './Hero/Hero';
+import Homepage from './Homepage/Homepage';
+import Footer from './Footer/Footer';
+import AboutPage from './AboutPage/AboutPage';
+import LoginPage from './LoginPage/Loginpage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <Router>
+
+    //  <div className='mainContainer'>
+    //   <Switch>
+    //     <Route path="/about">
+    //       <AboutPage/>
+    //     </Route>
+    //     <Route path="/login">
+    //       <LoginPage/>
+    //     </Route>
+    //     <Route path="/">
+    //       <Homepage/>
+    //     </Route>
+    //   </Switch>
+    //  </div>
+    //  <Footer></Footer>
+    // </Router>
+
+    <Router>
+      <div className="mainContainer">
+        <Menu/>
+        <Hero/>
+        <Routes>
+          <Route path='/' element={<Homepage/>} />
+          <Route path='/about' element={<AboutPage/>} />
+          <Route path='/login' element={<LoginPage/>} />
+        </Routes>
+        <Footer/>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
---
+++ src/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+    <div className="bottom">
+      <div className="center">
+        &copy; 2023 Sunil Jaithwalla. All rights reserved.
+      </div>
+    </div>
+  </footer>
+  );
+}
+
+export default Footer;
